8ebfe7dba60d77c7ea2a67113834befee4cd0c29
Vitor Silva <vitor.o.silva95@gmail.com>
2021-08-13 21:47:17 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+async function requestMerdadoLivre() {
+  const collection = [];
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const json = await response.json()
+  .then((data) => data.results);
+  json.forEach((currencyValue) => {
+  const computerObj = { 
+    sku: currencyValue.id,
+    name: currencyValue.title,
+    image: currencyValue.thumbnail,
+  };
+  collection.push(computerObj);
+});
+return collection;
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -29,7 +45,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  
+
   // coloque seu código aqui
 }
 
@@ -41,4 +57,14 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+async function fetchObj() {
+  const sectionItems = document.getElementsByClassName('items')[0];
+  const computerItems = await requestMerdadoLivre();
+  computerItems.forEach((element) => {
+    const createELementItem = createProductItemElement(element);
+    sectionItems.appendChild(createELementItem);
+    });
+  }
+
+// eslint-disable-next-line no-return-await
+window.onload = async () => await fetchObj();
